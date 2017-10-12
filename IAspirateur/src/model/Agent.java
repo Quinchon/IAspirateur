37dc2fa37c311dbf4c2abac7340cc573c0d726bf
Mri <Mri@132.207.203.49>
2017-10-12 19:23:02 -0400

    Changements sur Agent

--- IAspirateur/src/model/Agent.java
+++ IAspirateur/src/model/Agent.java
@@ -6,6 +6,7 @@ enum Action {
 	UP, DOWN, LEFT, RIGHT, SUCK, PICK, IDLE
 }
 
+
 public class Agent {
 	
 	private Environnement environnement;
@@ -20,8 +21,8 @@ public class Agent {
 	
 	ArrayList<Action> sequence_of_actions = new ArrayList<Action>();
 	
-	private Capteurs capteurs;
-	private Effecteurs effecteurs;
+	private Capteurs capteurs = new Capteurs();
+	private Effecteurs effecteurs = new Effecteurs();
 	
 	public Agent(Environnement environnement, int coordx, int coordy) {
 		this.environnement = environnement;
@@ -29,21 +30,54 @@ public class Agent {
 		this.coordy = coordy;
 	}
 	
-	public void observeEnvironmentWithAllMySensors() {
-		capteurs.scanEnvironnement(environnement, cases_with_dust, cases_with_jewel);
-		
+//Gérer les exceptions à l'avenir !!
+	public void takeAction(Action action) {
+		switch (action) {
+		case UP:
+			if (coordy < 9)
+				coordy ++;
+			break;
+		case DOWN:
+			if (coordy > 0)
+				coordy --;
+			break;
+		case LEFT:
+			if (coordx > 0)
+				coordx --;
+			break;
+		case RIGHT:
+			if (coordx < 9)
+				coordx ++;
+			break;
+		case SUCK:
+			effecteurs.suck(coordx, coordy, environnement);
+			break;
+		case PICK:
+			effecteurs.pickUp(coordx, coordy, environnement);
+			break;
+		case IDLE:
+			break;
+		}
 	}
 	
-	public void updateMyState() {
-		
+	
+	
+	public void observeEnvironmentWithAllMySensors() {
+		capteurs.scanEnvironnement(environnement, cases_with_dust, cases_with_jewel);
+		score = capteurs.getScore(environnement);
 	}
+	
+//	public void updateMyState() {
+//		
+//	}
 	
 	public void chooseAnAction(){
 		
 	}
 	
 	public void justDoIt() {
-		
+		takeAction(sequence_of_actions.get(0));
+		sequence_of_actions.remove(0);
 	}
 	
 }
--- IAspirateur/src/model/Effecteurs.java
+++ IAspirateur/src/model/Effecteurs.java
@@ -1,36 +1,13 @@
 package model;
 
 public class Effecteurs{
-
-	private enum Direction{
-		UP, DOWN, LEFT, RIGHT
+	
+	public void suck(int coordx, int coordy, Environnement environnement) {
+		environnement.suck(coordx, coordy);
 	}
 	
-	
-//Gérer les exceptions à l'avenir !!
-	public void move(Direction direction, int coordx, int coordy) {
-		switch (direction) {
-		case UP:
-			coordy ++;
-			break;
-		case DOWN:
-			coordy --;
-			break;
-		case LEFT:
-			coordx --;
-			break;
-		case RIGHT:
-			coordx ++;
-			break;
-		}
-	}
-	
-	public void suck() {
-		
-	}
-	
-	public void pickUp() {
-		
+	public void pickUp(int coordx, int coordy, Environnement environnement) {
+		environnement.pickUp(coordx, coordy);
 	}
 	
 }
