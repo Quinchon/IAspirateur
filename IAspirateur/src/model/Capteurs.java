d050f85f58ecc242aff2479f86adbee1a56e4603
Mri <Mri@DESKTOP-EO4I1AT>
2017-10-15 21:36:02 -0400

    Parcours en profondeur dans Capteurs

--- IAspirateur/src/model/Capteurs.java
+++ IAspirateur/src/model/Capteurs.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 /**
  * The sensors of the agent
@@ -40,94 +39,93 @@ public class Capteurs{
 		return environnement.getScore();
 	}
 	
-}
 	
-//	public ArrayList<Action> createSequence(Environnement environnement, int x, int y, int depth){
-//	
-//		sequence_of_actions.clear();
-//		
-//		if (!explore(environnement, sequence_of_actions, x, y, 0)) {
-//			sequence_of_actions.add(Action.IDLE);
-//		}
-//		
-//		return sequence_of_actions;
-//	}
-//	
-//	private boolean explore(Environnement environnement, ArrayList<Action> sequence_of_actions, int x, int y, int depth) {
-//		
-//		int case_marquees[][] = new int[10][10];
-//		for(int i = 0; i < 10; i++){
-//			   for(int j = 0; j < 10; j++){
-//			     case_marquees[i][j] = 0;  
-//			   }
-//			}
-//		
-//		
-//		Queue<Case> fileCase = new LinkedList<Case>();
-//		fileCase.add(environnement.getCase(x, y));
-//		case_marquees[x][y] = 1;
-//		
-//		
-//		if (environnement.hasJewel(x, y) || environnement.hasDust(x, y)) {
-//			if (environnement.hasJewel(x, y)) {
-//				sequence_of_actions.add(Action.PICK);
-//			}
-//			if (environnement.hasDust(x, y)) {
-//				sequence_of_actions.add(Action.SUCK);
-//			}
-//			return true;
-//		}
-//		
-//		
-//
-//		return false;
-//	}
-//	
+	public ArrayList<Action> createSequence(Environnement environnement, int x, int y){
+	
+		int max_depth = 1;
+		sequence_of_actions.clear();
+		
+		
+		while(!explore(environnement, x, y, 0, max_depth)) {
+			max_depth ++;
+		}
+		
+		if (sequence_of_actions.isEmpty()) {
+			sequence_of_actions.add(Action.IDLE);
+		}
+			
+		return sequence_of_actions;
+	}
+	
+	private boolean explore(Environnement environnement, int x, int y, int depth, int max_depth) {
+		
+		int case_marquees[][] = new int[10][10];
+		for(int i = 0; i < 10; i++){
+			   for(int j = 0; j < 10; j++){
+			     case_marquees[i][j] = 0;  
+			   }
+			}
+	
+		if (environnement.hasJewel(x, y) || environnement.hasDust(x, y)) {
+			if (environnement.hasJewel(x, y)) {
+				sequence_of_actions.add(Action.PICK);
+			}
+			if (environnement.hasDust(x, y)) {
+				sequence_of_actions.add(Action.SUCK);
+			}
+			return true;
+		}
+			
+		if (depth < 9) {
+		
+		//Explore UP
+		if (y > 0) {
+			sequence_of_actions.add(Action.UP);
+			if (explore(environnement, x, y - 1, depth + 1, max_depth)) {
+				return true;
+			}
+			else {
+				sequence_of_actions.remove(sequence_of_actions.size() - 1);
+			}
+		}
+		
+		//Explore DOWN
+		if (y < 9) {
+			sequence_of_actions.add(Action.DOWN);
+			if (explore(environnement, x, y + 1, depth + 1, max_depth)) {
+				return true;
+			}
+			else {
+				sequence_of_actions.remove(sequence_of_actions.size() - 1);
+			}
+		}		
+		
+		//Explore LEFT
+		if (x > 0) {
+			sequence_of_actions.add(Action.LEFT);
+			if (explore(environnement, x - 1, y, depth + 1, max_depth)) {
+				return true;
+			}
+			else {
+				sequence_of_actions.remove(sequence_of_actions.size() - 1);
+			}
+		}
+		
+		//Explore RIGHT
+		if (x < 9) {
+			sequence_of_actions.add(Action.RIGHT);
+			if (explore(environnement, x + 1, y, depth + 1, max_depth)) {
+				return true;
+			}
+			else {
+				sequence_of_actions.remove(sequence_of_actions.size() - 1);
+			}
+		}
+	
+	}
+		
+		return false;
+	}
+	
+}
 
-
-//if (depth < 9) {
-//	
-//	//Explore UP
-//	if (y > 0) {
-//		sequence_of_actions.add(Action.UP);
-//		if (explore(environnement, sequence_of_actions, x, y - 1, depth + 1)) {
-//			return true;
-//		}
-//		else {
-//			sequence_of_actions.remove(sequence_of_actions.size() - 1);
-//		}
-//	}
-//	//Explore DOWN
-//	if (y < 9) {
-//		sequence_of_actions.add(Action.DOWN);
-//		if (explore(environnement, sequence_of_actions, x, y + 1, depth + 1)) {
-//			return true;
-//		}
-//		else {
-//			sequence_of_actions.remove(sequence_of_actions.size() - 1);
-//		}
-//	}		
-//	//Explore LEFT
-//	if (x > 0) {
-//		sequence_of_actions.add(Action.LEFT);
-//		if (explore(environnement, sequence_of_actions, x - 1, y, depth + 1)) {
-//			return true;
-//		}
-//		else {
-//			sequence_of_actions.remove(sequence_of_actions.size() - 1);
-//		}
-//	}
-//	//Explore RIGHT
-//	if (x < 9) {
-//		sequence_of_actions.add(Action.RIGHT);
-//		if (explore(environnement, sequence_of_actions, x + 1, y, depth + 1)) {
-//			return true;
-//		}
-//		else {
-//			sequence_of_actions.remove(sequence_of_actions.size() - 1);
-//		}
-//	}
-//
-//}
-//
-//return false;
